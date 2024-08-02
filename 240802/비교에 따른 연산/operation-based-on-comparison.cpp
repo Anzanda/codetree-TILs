6a107e4beeb8292d95957aca29d437a44174f929
YoonSeong Lee <79851762+Anzanda@users.noreply.github.com>
2024-08-02 23:53:14 +0900

    Title: 비교에 따른 연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240802/비교에 따른 연산/operation-based-on-comparison.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b;
+    cin >> a >> b;
+    if(a > b) {
+        cout << a*b << endl;
+     } else {
+        cout << (b/a) << endl;
+     }
+    return 0;
+}
