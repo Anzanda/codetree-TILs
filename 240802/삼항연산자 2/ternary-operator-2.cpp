99168dd24b3d7668da55bec0a56a00a8c6f94074
YoonSeong Lee <79851762+Anzanda@users.noreply.github.com>
2024-08-02 23:53:35 +0900

    Title: 삼항연산자 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240802/삼항연산자 2/ternary-operator-2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    cin >> a;
+    if(a == 1) cout << "t" << endl;
+    else cout << "f" << endl;
+    return 0;
+}
