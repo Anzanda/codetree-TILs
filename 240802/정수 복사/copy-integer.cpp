ea3cdf73500ceea2dfd95393b9766e12ac443c0d
YoonSeong Lee <79851762+Anzanda@users.noreply.github.com>
2024-08-02 23:46:36 +0900

    Title: 정수 복사, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240802/정수 복사/copy-integer.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    auto a = 3;
+    auto b = 4;
+    b = a;
+
+    cout << a << ' ' << b << endl;
+    cout << a * b << endl;
+    
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
