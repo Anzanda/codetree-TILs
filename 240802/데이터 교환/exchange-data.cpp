b6dc70e290aa215e0ad476170822df82eb11ea0a
YoonSeong Lee <79851762+Anzanda@users.noreply.github.com>
2024-08-02 23:47:10 +0900

    Title: 데이터 교환, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240802/데이터 교환/exchange-data.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    auto a = 5;
+    auto b = 6;
+    auto c = 7;
+
+    cout << 7 << endl;
+    cout << 5 << endl;
+    cout << 6 << endl;
+    return 0;
+}
