7154a7368fe802bbe5425a07bb4e24b3b0a901f0
YoonSeong Lee <79851762+Anzanda@users.noreply.github.com>
2024-08-02 23:44:13 +0900

    Title: 변수 출력하기 3, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240802/변수 출력하기 3/outputing-variables-3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a = 1;
+    int b = 2;
+    int c = 'C'
+    cout << a << "->" << b << "->" << c << endl;
+    return 0;
+}
