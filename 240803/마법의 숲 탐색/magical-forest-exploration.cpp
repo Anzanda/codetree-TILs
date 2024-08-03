4f0f4f2a5383ce47dbe45eef2b6e4b226ee999df
YoonSeong Lee <79851762+Anzanda@users.noreply.github.com>
2024-08-03 20:21:42 +0900

    Title: 마법의 숲 탐색, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240803/마법의 숲 탐색/magical-forest-exploration.cpp
+++ 240803/마법의 숲 탐색/magical-forest-exploration.cpp
@@ -1,61 +1,74 @@
 #include <bits/stdc++.h>
 #define endl '\n'
 using namespace std;
-using pii = pair<int,int>;
+
 const int MAX = 70+7;
 
-
+//612
 int n, m, k;
 int dr[4] = {-1, 0, 1, 0};
 int dc[4] = {0, 1, 0, -1};
-int golem[MAX][MAX];
-bool is_exit[MAX][MAX];
-bool is_golem_range(int r, int c) {
-    return r >= -1 && r <= n-1 && c >= 2 && c <= m-1;
-}
-bool can_alive(int r, int c) {
-    return r >= 2 && r <= n-1 && c >= 2 && c <= m-1;
+bool vst[MAX][MAX];
+bool is_range(int r, int c) {
+    return r>=0 && r<= n && c >= 1 && c <= m;
 }
 bool is_valid(int r, int c) {
-    if(!is_golem_range(r, c)) return false;
+    if(!is_range(r, c)) return false;
+    if(vst[r][c]) return false;
     for(int i=0; i<4; i++) {
         int nr = r + dr[i];
         int nc = c + dc[i];
-        if(nr <= -1) continue;
-        if(golem[nr][nc] != -1) return false;
-    }    
+        if(nr < 0) continue;
+        if(!is_range(nr, nc)) return false;
+        if(vst[nr][nc]) return false;
+    }
     return true;
 }
 int ans;
 int tmp_ans;
-bool vst[MAX][MAX];
-bool is_range(int r, int c) {
-    return r>=1 && r<=n && c>=1 && c<=m;
+bool vst2[MAX][MAX];
+void dfs(int r, int c) {
+    vst2[r][c] = true;
+
+    tmp_ans = max(tmp_ans, r);
+    for(int i=0; i<4; i++) {
+        int nr = r + dr[i];
+        int nc = c + dc[i];
+        if(!is_range(nr, nc)) continue;
+        if(!vst[nr][nc]) continue;
+        if(vst2[nr][nc]) continue;
+        dfs(nr, nc);
+    }
 }
-// void dfs(int r, int c) {
-//     vst[r][c] = true;
-//     tmp_ans = max(tmp_ans, r);
-//     for(int i=0; i<4; i++) {
-//         int nr = r + dr[i];
-//         int nc = c + dc[i];
-//         if(!is_range(nr, nc)) continue;
-//         if(!golem[nr][nc]) continue;
-//         if(vst[nr][nc]) continue;
-//         dfs(nr, nc);
-//     }
-// }
 void run() {
-    int r, c, d;
+    int c, d;
     cin >> c >> d;
 
-    r = -1;
+    int r = 0;
+    if(vst[r+1][c]) {
+       if(is_range(r, c-2) && (!vst[r+1][c-1])) {
+        c -= 1;
+        d = (d-1)%4;
+       }
+       else if(is_range(r, c+2) && (!vst[r+1][c+1])) {
+         c += 1;
+         d = (d+1)%4;
+       }
+       else {
+        memset(vst, false, sizeof vst);
+        return;
+       }
+    }
     
+    // r == 0
     bool alive = true;
     while(alive) {
+        // 1
         if(is_valid(r+1, c)) {
             r++;
             continue;
         }
+        // 2
         if(is_valid(r, c-1) && is_valid(r+1, c-1)) {
             r++;
             c--;
@@ -71,55 +84,34 @@ void run() {
         alive = false;
     }
 
-    if(!can_alive(r, c)) {
-        memset(golem, -1, sizeof golem);
-        memset(is_exit, false, sizeof is_exit);
-        return;
-    }
-
-
-    tmp_ans = r + 1;
-
-    golem[r][c] = k;
     for(int i=0; i<4; i++) {
         int nr = r + dr[i];
         int nc = c + dc[i];
-        golem[nr][nc] = k;
-    }
-    is_exit[r+dr[d]][c+dc[d]] = true;
-
-    memset(vst, false, sizeof vst);
-    queue<pii> q;
-    q.push(pii(r, c));
-    while(q.size()) {
-        auto [rr, cc] = q.front();
-        q.pop();
-
-        vst[rr][cc] = true;
-        tmp_ans = max(tmp_ans, rr);
-        for(int i=0;  i<4; i++) {
-            int nr = rr + dr[i];
-            int nc = cc + dc[i];
-            if(!is_range(nr, nc)) continue;
-            if(vst[nr][nc]) continue;
-            if((golem[rr][cc] == golem[nr][nc]) || (is_exit[rr][cc] && golem[nr][nc] != -1)) 
-                q.push(pii(nr, nc));
+        if(!is_range(nr, nc) || nr == 0) {
+            memset(vst, false, sizeof vst);
+            return;
         }
     }
-    
-    // printf("tmp_ans: %d\n", tmp_ans);
-    // printf("r: %d, c: %d\n", r, c);
-    ans += tmp_ans;
 
-  
-    
+    tmp_ans = r+1;
+    memset(vst2, false, sizeof vst2);
+    dfs(r+dr[d], c+dc[d]);
+    ans += tmp_ans;
+    vst[r][c] = true;
+    for(int i=0; i<4; i++) {
+        int nr = r + dr[i];
+        int nc = c + dc[i];
+        vst[nr][nc] = true;
+    }
 }
-int main(void) {
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
     cin >> n >> m >> k;
-    memset(golem, -1, sizeof golem);
+
     while(k--) {
         run();
     }
-
     cout << ans << endl;
+    // 여기에 코드를 작성해주세요.
+    return 0;
 }
