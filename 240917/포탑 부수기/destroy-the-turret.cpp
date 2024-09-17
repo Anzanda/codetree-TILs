4758ccbabffc9ab34d6adeda36a9db02f010cee1
YoonSeong Lee <79851762+Anzanda@users.noreply.github.com>
2024-09-17 13:29:41 +0900

    Title: 포탑 부수기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240917/포탑 부수기/destroy-the-turret.cpp
+++ 240917/포탑 부수기/destroy-the-turret.cpp
@@ -1,9 +1,9 @@
 /******************************************************************************
 
 Welcome to GDB Online.
-  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
-  Code, Compile, Run and Debug online from anywhere in world.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
 #include <bits/stdc++.h>
@@ -12,13 +12,11 @@ Welcome to GDB Online.
 using namespace std;
 using pii = pair<int,int>;
 
-const int MAXN = 10+7;
+const int MAX = 10+7;
 
 int N, M, K;
-int a[MAXN][MAXN];
-int attack_turn[MAXN][MAXN];
-
-
+int a[MAX][MAX];
+int attack_turn[MAX][MAX];
 int turn;
 pii find_attacker() {
     int min_attack = 987654321;
@@ -29,108 +27,109 @@ pii find_attacker() {
         }
     }
     
-    vector<pii> cand; 
+    vector<pii> min_attacks;
     for(int i=1; i<=N; i++) {
         for(int j=1; j<=M; j++) {
             if(a[i][j] <= 0) continue;
             if(min_attack == a[i][j]) {
-                cand.push_back(pii(i, j));
+               min_attacks.push_back(pii(i,j)); 
             }
         }
     }
     
-    int max_attack_turn = -12;
-    for(auto loc : cand) {
-        auto [r, c] = loc;
-        max_attack_turn = max(max_attack_turn, attack_turn[r][c]);
+    int max_turn = -1;
+    for(auto cand : min_attacks) {
+        auto [r, c] = cand;
+        max_turn = max(max_turn, attack_turn[r][c]);
     }
-    vector<pii> cand2;
-    for(auto loc : cand) {
-        auto [r, c] = loc;
-        if(max_attack_turn == attack_turn[r][c]) {
-            cand2.push_back(pii(r,c));
+    vector<pii> max_turns;
+    for(auto cand : min_attacks) {
+        auto [r, c] = cand;
+        if(max_turn == attack_turn[r][c]) {
+            max_turns.push_back(pii(r,c));
         }
     }
     
-    int max_sum = -12;
-    for(auto loc : cand2) {
-        auto [r, c] = loc;
-        max_sum = max(max_sum, r+c);
+    int max_sum = -1;
+    for(auto cand : max_turns) {
+        auto [r, c] = cand;
+        max_sum = max(max_sum, r + c);
     }
-    vector<pii> cand3;
-    for(auto loc : cand2) {
-        auto [r, c] = loc;
+    vector<pii> max_sums;
+    for(auto cand : max_turns) {
+        auto [r, c] = cand;
         if(max_sum == (r+c)) {
-            cand3.push_back(pii(r, c));
+            max_sums.push_back(pii(r,c)); 
         }
     }
     
-    int max_c = -12;
-    for(auto loc : cand3) {
-        auto [r, c] = loc;
+    int max_c = -1;
+    for(auto cand : max_sums) {
+        auto [r, c] = cand;
         max_c = max(max_c, c);
     }
-    for(auto loc: cand3) {
-        auto [r, c] = loc;
+    for(auto cand : max_sums) {
+        auto [r, c] = cand;
         if(max_c == c) {
             return pii(r, c);
         }
     }
+    
     return pii(-1, -1);
 }
 pii find_defender(pii attacker) {
-    int max_attack = -12;
+    int max_attack = -1;
     for(int i=1; i<=N; i++) {
         for(int j=1; j<=M; j++) {
             if(a[i][j] <= 0) continue;
+            if(i == attacker.first && j == attacker.second) continue;
             max_attack = max(max_attack, a[i][j]);
         }
     }
-    
-    vector<pii> cand; 
+    vector<pii> max_attacks;
     for(int i=1; i<=N; i++) {
         for(int j=1; j<=M; j++) {
             if(a[i][j] <= 0) continue;
+            if(i == attacker.first && j == attacker.second) continue;
             if(max_attack == a[i][j]) {
-                if(i == attacker.first && j == attacker.second) continue;
-                cand.push_back(pii(i, j));
+               max_attacks.push_back(pii(i,j)); 
             }
         }
     }
     
-    int min_attack_turn = 987654321;
-    for(auto loc : cand) {
-        auto [r, c] = loc;
-        min_attack_turn = min(min_attack_turn, attack_turn[r][c]);
+    int min_turn = INF;
+    for(auto cand : max_attacks) {
+        auto [r, c] = cand;
+        min_turn = min(min_turn, attack_turn[r][c]);
     }
-    vector<pii> cand2;
-    for(auto loc : cand) {
-        auto [r, c] = loc;
-        if(min_attack_turn == attack_turn[r][c]) {
-            cand2.push_back(pii(r, c));
+    vector<pii> min_turns;
+    for(auto cand : max_attacks) {
+        auto [r, c] = cand;
+        if(min_turn == attack_turn[r][c]) {
+            min_turns.push_back(pii(r,c));
         }
     }
     
-    int min_sum = 987654321;
-    for(auto loc : cand2) {
-        auto [r, c] = loc;
-        min_sum = min(min_sum, r+c);
+    int min_sum = INF;
+    for(auto cand : min_turns) {
+        auto [r, c] = cand;
+        min_sum = min(min_sum, r + c);
     }
-    vector<pii> cand3;
-    for(auto loc : cand2) {
-        auto [r, c] = loc;
+    vector<pii> min_sums;
+    for(auto cand : min_turns) {
+        auto [r, c] = cand;
         if(min_sum == (r+c)) {
-            cand3.push_back(pii(r, c));
+            min_sums.push_back(pii(r,c)); 
         }
     }
     
-    int min_c = 98765431;
-    for(auto loc : cand3) {
-        auto [r, c] = loc;
+    int min_c = INF;
+    for(auto cand : min_sums) {
+        auto [r, c] = cand;
         min_c = min(min_c, c);
     }
-    for(auto loc: cand3) {
-        auto [r, c] = loc;
+    for(auto cand : min_sums) {
+        auto [r, c] = cand;
         if(min_c == c) {
             return pii(r, c);
         }
@@ -138,62 +137,27 @@ pii find_defender(pii attacker) {
     
     return pii(-1, -1);
 }
-
+void apply_handicap(pii attacker) {
+    auto [r, c] = attacker;
+    
+    attack_turn[r][c] = turn;
+    a[r][c] += (N + M);
+}
 int dr[4] = {0, 1, 0, -1};
 int dc[4] = {1, 0, -1, 0};
-pii move(pii curr, int d) {
-    auto [r,c] = curr;
-    int nr = r + dr[d];
-    int nc = c + dc[d];
+pii move(int r, int c) {
+    if(r > N) r = 1;
+    if(r < 1) r = N;
+    if(c > M) c = 1;
+    if(c < 1) c = M;
     
-    if(nr > N) nr = 1;
-    if(nr < 1) nr = N;
-    if(nc > M) nc = 1;
-    if(nc < 1) nc = M;
-    
-    return pii(nr, nc);
+    return pii(r, c);
 }
-int dp[MAXN][MAXN];
-int dfs(pii curr, pii defender) {
-    auto [r, c] = curr;
-    
-    int &ret = dp[r][c];
-    if(ret != -1) {
-        return ret;
-    }
-    
-    if(r == defender.first && c == defender.second) {
-        return ret = 0;
-    }
-    
-    
-    ret = INF;
-    for(int i=0; i<4; i++) {
-        auto [nr, nc] = move(curr, i);
-        if(a[nr][nc] <= 0) continue;
-        ret = min(ret, 1+dfs(pii(nr,nc), defender));
-    }
-    
-    return ret;
-}
+int back_r[MAX][MAX];
+int back_c[MAX][MAX];
 bool can_raser(pii attacker, pii defender) {
-    memset(dp, -1, sizeof dp);
-    int ret = dfs(attacker, defender);
-    
-    return (ret != INF);
-}
-bool vst[MAXN][MAXN];
-void do_raser(pii attacker, pii defender) {
-    // for(int i=1; i<=N; i++) {
-    //     for(int j=1; j<=M; j++) {
-    //         cout << dp[i][j] << ' ';
-    //     }
-    //     cout << endl;
-    // } 
+    bool vst[MAX][MAX];
     memset(vst, false, sizeof vst);
-    
-    int damage = a[attacker.first][attacker.second];
-    a[attacker.first][attacker.second] += (damage/2);
     
     queue<pii> q;
     q.push(attacker);
@@ -201,105 +165,99 @@ void do_raser(pii attacker, pii defender) {
         auto [r, c] = q.front();
         q.pop();
         
-        vst[r][c] = true;
         if(r == defender.first && c == defender.second) {
-            a[r][c] -= damage;
-            break;
-        } else {
-            a[r][c] -= (damage/2);
+            return true;
         }
+        
         for(int i=0; i<4; i++) {
-            auto [nr, nc] = move(pii(r, c), i);
-            if(dp[r][c] - 1 == dp[nr][nc]) {
-                q.push(pii(nr, nc));
-                break;
-            }
+           auto [nr, nc] = move(r + dr[i], c + dc[i]); 
+           if(a[nr][nc] <= 0) continue;
+           if(vst[nr][nc]) continue;
+           
+           vst[nr][nc] = true;
+           back_r[nr][nc] = r;
+           back_c[nr][nc] = c;
+           q.push(pii(nr, nc));
         }
     }
+    
+    return false;
 }
-pii move_without_dir(int r, int c) {
-    int nr = r;
-    int nc = c;
+bool is_affect[MAX][MAX];
+void go_raser(pii attacker, pii defender) {
+    memset(is_affect, false, sizeof is_affect);
     
-    if(nr > N) nr = 1;
-    if(nr < 1) nr = N;
-    if(nc > M) nc = 1;
-    if(nc < 1) nc = M;
-    
-    return pii(nr, nc);
-}
-void potan(pii attacker, pii defender) {
-    memset(vst, false, sizeof vst);
-    
-    int dr[8] = {-1, -1, -1, 0, 1, 1, 1, 0};
-    int dc[8] = {-1, 0, 1, 1, 1, 0, -1, -1};
-    
-    int damage = a[attacker.first][attacker.second];
-    vst[attacker.first][attacker.second] = true;
+    int damage = a[attacker.first][attacker.second]; 
+    is_affect[attacker.first][attacker.second] = true;
     
     auto [r, c] = defender;
     a[r][c] -= damage;
-    vst[r][c] = true;
+    is_affect[r][c] = true;
     
-    for(int i=0; i<8; i++) {
-        auto [nr, nc] = move_without_dir(r + dr[i], c + dc[i]);
-        if(a[nr][nc] <= 0) continue;
-        vst[nr][nc] = true;
-        if(nr == attacker.first && nc == attacker.second) {
-            continue;
-        }
+    int nr = back_r[r][c];
+    int nc = back_c[r][c];
+    while(!(nr == attacker.first && nc == attacker.second)) {
         a[nr][nc] -= (damage/2);
+        is_affect[nr][nc] = true; 
+        
+        int nnr = back_r[nr][nc];
+        int nnc = back_c[nr][nc];
+        
+        nr = nnr;
+        nc = nnc;
+    }
+}
+void potan(pii attacker, pii defender) {
+    memset(is_affect, false, sizeof is_affect);
+    
+    int damage = a[attacker.first][attacker.second]; 
+    is_affect[attacker.first][attacker.second] = true;
+    
+    auto [r, c] = defender;
+    a[r][c] -= damage;
+    is_affect[r][c] = true;
+    
+    int dr[8] = {-1, -1, -1, 0, 1, 1, 1, 0};
+    int dc[8] = {-1, 0, 1, 1, 1, 0, -1, -1};
+    for(int i=0; i<8; i++) {
+       auto [nr, nc] = move(r + dr[i], c + dc[i]); 
+       is_affect[nr][nc] = true;
+       if(nr == attacker.first && nc == attacker.second) {
+           continue;
+       }
+       if(nr == defender.first && nc == defender.second) {
+           continue;
+       }
+       if(a[nr][nc] <= 0) continue;
+       
+       a[nr][nc] -= (damage/2);
     }
 }
 void jungbi() {
     for(int i=1; i<=N; i++) {
         for(int j=1; j<=M; j++) {
-            if(vst[i][j]) continue;
             if(a[i][j] <= 0) continue;
+            if(is_affect[i][j]) continue;
             a[i][j]++;
         }
-    } 
-}
-bool is_finished() {
-    int cnt = 0;
-    for(int i=1; i<=N; i++) {
-        for(int j=1; j<=M; j++) {
-            if(a[i][j] > 0) {
-                cnt++;
-            }
-        }
     }
-    return (cnt == 1);
 }
-void go_turn() {
-    auto [r, c]= find_attacker();   
-    pii defender = find_defender(pii(r,c));
-    
-    attack_turn[r][c] = turn;
-    a[r][c] += (N+M);
-    if(can_raser(pii(r, c), defender)) {
-        do_raser(pii(r, c), defender); 
+void do_turn() {
+   pii attacker = find_attacker();     
+   pii defender = find_defender(attacker);
+   
+   apply_handicap(attacker);
+   
+    if(can_raser(attacker, defender)) {
+       go_raser(attacker, defender); 
     } else {
-        potan(pii(r, c), defender);
+       potan(attacker, defender); 
     }
+    
     jungbi();
-    if(is_finished()) {
-        K = 0;
-        return;
-    }
-    // cout << "**********" << endl;
-    // for(int i=1; i<=N; i++) {
-    //     for(int j=1; j<=M; j++) {
-    //         cout << a[i][j] << ' ';
-    //     }
-    //     cout << endl;
-    // }
-    // cout << "**********" << endl;
 }
 int main()
 {
-    cin.tie(0)->sync_with_stdio(0);
-    
     cin >> N >> M >> K;
     for(int i=1; i<=N; i++) {
         for(int j=1; j<=M; j++) {
@@ -307,10 +265,9 @@ int main()
         }
     }
     
-    memset(attack_turn, -1, sizeof attack_turn);
     while(K--) {
-        go_turn();
         turn++;
+        do_turn();
     }
     
     int ans = -1;
@@ -319,7 +276,7 @@ int main()
             ans = max(ans, a[i][j]);
         }
     }
-    
+
     cout << ans << endl;
     return 0;
 }
